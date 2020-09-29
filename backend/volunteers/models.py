616d9f850ee0a6305817fcb8d33dc37af50f53cd
unknown <diebraga0055@gmail.com>
2020-09-29 12:07:35 +0100

    added volunteer models and serializers

--- backend/volunteers/models.py
+++ backend/volunteers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Volunteer(models.Model):
+    """Model for volunteer creation"""
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%d/%m/%y/')
+    age = models.IntegerField(max_length=3)
+    degree = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    experience_volunteering = models.TextField(blank=True)
+    date_register = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
