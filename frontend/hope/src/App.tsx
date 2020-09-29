e8b7e5016d74f0fd9c969fb545bdfe20a0c2bf90
unknown <diebraga0055@gmail.com>
2020-09-29 23:53:29 +0100

    added reducers + types & store

--- frontend/hope/src/App.tsx
+++ frontend/hope/src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
 import Routes from './routes';
-
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
+import store from './store';
+
 const App: React.FC = () => (
   <>
-    <GlobalStyle />
-    <Header />
+    <Provider store={store}>
+      <GlobalStyle />
+      <Header />
 
-    <Router>
-      <Routes />
-    </Router>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
   </>
 );
 
