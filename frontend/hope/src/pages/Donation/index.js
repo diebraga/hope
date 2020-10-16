b24c1c57bcfbc75c320fe277c249e02ca06f9fb4
unknown <diebraga0055@gmail.com>
2020-10-16 19:11:41 +0100

    structure 1 finished

--- frontend/hope/src/pages/Donation/index.js
+++ frontend/hope/src/pages/Donation/index.js
@@ -1,66 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+import { loadStripe } from "@stripe/stripe-js";
 import {
   Elements,
   CardElement,
   useStripe,
   useElements,
-} from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+} from "@stripe/react-stripe-js";
 
-// the key is located in the .env file
-const stripePromise = loadStripe(
-  'pk_live_51HV03zJuRjbktOF0CQ08Ng2fz2cRuu81ebLxecY9ZRLLak7JS8sLSFxmR2Y0mpUDlVhM0y4QVUsRFKsXUilUBkNf00o1ofbu6o',
-);
+import axios from "axios";
 
-// import { Container } from './styles';
+const stripePromise = loadStripe("pk_live_51HV03zJuRjbktOF0CQ08Ng2fz2cRuu81ebLxecY9ZRLLak7JS8sLSFxmR2Y0mpUDlVhM0y4QVUsRFKsXUilUBkNf00o1ofbu6o");
 
-const Donation = () => {
-  const [email, setEmail] = useState('');
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
 
-  const CheckoutForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type: 'card',
-        card: elements.getElement(CardElement),
-      });
-    };
-    return (
-      <form>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4',
-                },
-              },
-              invalid: {
-                color: '#9e2146',
-              },
-            },
-          }}
-        />
-        <button type="submit" className="">
-          Donate
-        </button>
-      </form>
-    );
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    setLoading(true);
+
+    if (!error) {
+      // console.log(paymentMethod)
+      const { id } = paymentMethod;
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            amount: 10000, //cents
+          }
+        );
+        console.log(data);
+
+        elements.getElement(CardElement).clear();
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
   };
 
+  console.log(!stripe || loading);
+
   return (
-    <>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm />
-      </Elements>
-    </>
+    <form className="card card-body" onSubmit={handleSubmit}>
+      {/* Product Information */}
+
+      <h3 className="text-center my-2">Price: ?$</h3>
+
+      {/* User Card Input */}
+      <div className="form-group">
+        <CardElement />
+      </div>
+
+      <button disabled={!stripe} className="btn btn-success">
+        {loading ? (
+          <div className="spinner-border text-light" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          "Donate"
+        )}
+      </button>
+    </form>
   );
 };
 
-export default Donation;
+function App() {
+  return (
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
+        <div className="row h-100">
+          <div className="col-md-4 offset-md-4 h-100">
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
+  );
+}
+
+export default App;
