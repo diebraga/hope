2e9766fc1fe35459382e93a0a6f78c4249430f19
unknown <diebraga0055@gmail.com>
2020-10-19 16:39:05 +0100

    struture 4 products

---
+++ frontend/hope/src/components/Checkout.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import axios from 'axios'
+
+const CheckoutForm = (props) => {
+
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [checkoutErrorMsg, setCheckoutErrorMsg] = useState('')
+    const [buttonMsg, setButtonMsg] = useState("Donate")
+
+    const stripe = useStripe()
+    const element = useElements()
+
+    const handleChange = (e) => {
+        if(e.error){
+            return setCheckoutErrorMsg(e.error.message)
+        }
+        setCheckoutErrorMsg("Invalid try again!")
+    }
+
+    const handlePayment = async (e) => {
+
+        e.preventDefault()
+
+        setIsProcessing(true)
+        setButtonMsg("Processing...")
+
+        const cardElement = element.getElement('card')
+
+        const billingInfo = {
+            name: e.target.name.value,
+            phone: e.target.phone.value,
+            email: e.target.email.value,
+            address: {
+                line1: e.target.address.value
+            }
+        }
+
+        try {
+            // Got our client secret
+            const paymentIntent = await axios.post("http://localhost:5000/payment", {
+                amount: props.product.price * 100
+            })
+
+            // Create PaymentMethod Object
+            const paymentMethodObj = await stripe.createPaymentMethod({
+                type: "card",
+                card: cardElement,
+                billing_details: billingInfo
+            })
+
+            if(paymentMethodObj.error){
+                setCheckoutErrorMsg(paymentMethodObj.error.message)
+                setIsProcessing(false)
+                setButtonMsg("Donate")
+                return
+            }
+
+            // Confirm Payment Method
+            const confirmPayment = await stripe.confirmCardPayment(paymentIntent.data, {
+                payment_method: paymentMethodObj.paymentMethod.id
+            })
+
+            if(confirmPayment.error){
+                setCheckoutErrorMsg(confirmPayment.error.message)
+                setIsProcessing(false)
+                setButtonMsg("Donate")
+                return
+            }
+
+            setButtonMsg("Success! Payment is Complete")
+
+            setTimeout(() => {
+                setButtonMsg("Donate")
+                setIsProcessing(false)
+            }, 2000)
+
+        } catch (error) {
+            setCheckoutErrorMsg(error.message)
+            setIsProcessing(false)
+        }
+
+    }
+
+    return (
+        <div className="CheckoutForm">
+            <h3 className="purchase-msg">
+                You are Donating <span>{props.product.name}</span>${props.product.price}&nbsp;
+            </h3>
+            <form className="form" onSubmit={handlePayment}>
+                <input
+                    type="text"
+                    placeholder="Full name"
+                    name="name"
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Phone number"
+                    name="phone"
+                    required
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Address"
+                    name="address"
+                    required
+                />
+                <p>
+                    {checkoutErrorMsg}
+                </p>
+                <CardElement
+                    options={{
+                        hidePostalCode: true,
+                        style: {
+                            base: {
+                                fontSize: '20px'
+                            }
+                        }
+                    }}
+                    onChange={handleChange}
+                />
+                <button type="submit" disabled={isProcessing}>
+                    {buttonMsg}
+                </button>
+            </form>
+        </div>
+    )
+}
+
+
+export default CheckoutForm
--- frontend/hope/src/pages/Donation/index.js
+++ frontend/hope/src/pages/Donation/index.js
@@ -1,91 +1,70 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
+import ProductCard from "../../components/Product"
+import CheckoutForm from "../../components/Checkout"
+import {Elements} from "@stripe/react-stripe-js"
+import {loadStripe} from "@stripe/stripe-js"
 
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  Elements,
-  CardElement,
-  useStripe,
-  useElements,
-} from "@stripe/react-stripe-js";
+const stripePromise = loadStripe('pk_live_51HV03zJuRjbktOF0CQ08Ng2fz2cRuu81ebLxecY9ZRLLak7JS8sLSFxmR2Y0mpUDlVhM0y4QVUsRFKsXUilUBkNf00o1ofbu6o')
 
-import axios from "axios";
+stripePromise.then(data => {
+  console.log(data)
+})
 
-const stripePromise = loadStripe("pk_live_51HV03zJuRjbktOF0CQ08Ng2fz2cRuu81ebLxecY9ZRLLak7JS8sLSFxmR2Y0mpUDlVhM0y4QVUsRFKsXUilUBkNf00o1ofbu6o");
+const Donation = () => {
 
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
-    setLoading(true);
-
-    if (!error) {
-      // console.log(paymentMethod)
-      const { id } = paymentMethod;
-      try {
-        const { data } = await axios.post(
-          "http://localhost:3001/api/checkout",
-          {
-            id,
-            amount: 10000, //cents
-          }
-        );
-        console.log(data);
-
-        elements.getElement(CardElement).clear();
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
+  const [products, setProducts] = useState([
+    {
+    img: "http",
+    name: "",
+    price: 50
+    },
+    {
+      img: "",
+      name: "",
+      price: 100
+    },
+    {
+      img: "",
+      name: "",
+      price: 200
+    },
+    {
+      img: "",
+      name: "",
+      price: 400
     }
-  };
+  ])
 
-  console.log(!stripe || loading);
+  const [product, setProduct] = useState({
+    name: "Organic Apple",
+    price: 70
+  })
+
+  const displayProducts = () => {
+    return products.map(product => {
+      return (
+        <ProductCard
+          product={product}
+          setProduct={setProduct}
+        />
+      )
+    })
+  }
 
   return (
-    <form className="card card-body" onSubmit={handleSubmit}>
-      {/* Product Information */}
-
-      <h3 className="text-center my-2">Price: ?$</h3>
-
-      {/* User Card Input */}
-      <div className="form-group">
-        <CardElement />
-      </div>
-
-      <button disabled={!stripe} className="btn btn-success">
-        {loading ? (
-          <div className="spinner-border text-light" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        ) : (
-          "Donate"
-        )}
-      </button>
-    </form>
-  );
-};
-
-function App() {
-  return (
-    <Elements stripe={stripePromise}>
-      <div className="container p-4">
-        <div className="row h-100">
-          <div className="col-md-4 offset-md-4 h-100">
-            <CheckoutForm />
-          </div>
+    <Elements
+      stripe={stripePromise}
+    >
+      <div className="container-fluid">
+        <div className="">
+          {displayProducts()}
         </div>
+        <CheckoutForm
+          product={product}
+        />
       </div>
     </Elements>
   );
 }
 
-export default App;
+export default Donation;
